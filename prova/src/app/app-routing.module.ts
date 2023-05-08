4bab78ee1efb10f3e3c3bacca9e4ca4c695779e1
Mattia-Ottaviano <ottaviano.mattia@iisgalvanimi.edu.it>
2023-05-08 04:29:12 +0000

    2

--- prova/src/app/app-routing.module.ts
+++ prova/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemComponent } from './item/item.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'item-formula1/:id', component: SearchComponent },
+  { path: 'search-formula1', component: ItemComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
