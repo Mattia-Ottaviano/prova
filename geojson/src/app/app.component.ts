cc54fb798748a4f5b9f3b20e634c2d9374efa999
Mattia-Ottaviano <ottaviano.mattia@iisgalvanimi.edu.it>
2023-05-20 16:37:14 +0000

    finish

---
+++ geojson/src/app/app.component.ts
@@ -0,0 +1,89 @@
+//aggiungo ViewChild
+import { Component, ViewChild } from '@angular/core';
+
+//aggiungo:
+import { AfterViewInit } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps'
+import { GEOJSON, GeoFeatureCollection } from './models/geojson.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+//implemento AfterViewInit
+export class AppComponent implements AfterViewInit{
+
+  //Aggiungiamo latitudine e longitudine di un luogo
+  center: google.maps.LatLngLiteral = { lat: 45.506738, lng: 9.190766 };
+  geoJsonObject : GeoFeatureCollection;
+  fillColor: string = "#FF0000";  //Colore delle zone catastali
+  markerList : google.maps.MarkerOptions[];
+  zoom = 8;
+
+  constructor() {
+
+    //Questi dati dovremmo scaricarli dal server, per ora li abbiamo copiati nel file gojson.model.ts
+    this.geoJsonObject = GEOJSON;  
+    console.log(this.geoJsonObject); //stampo l'oggetto geoJsonObject sulla console
+    this.markerGenerator()
+  }
+
+  //utilizziamo il decoratore ViewChild per visualizzare i dati
+  @ViewChild('mapRef') mapRef: GoogleMap;
+  ngAfterViewInit(): void {
+    this.mapRef.data.addGeoJson(this.geoJsonObject);
+    //coloriamo la zona catastale
+    this.mapRef.data.setStyle(this.styleFunc)
+    
+  }
+  title = 'geojson';
+  
+  //coloriamo la zona catastale
+  styleFunc = (feature:any) =>{
+    console.log(feature.h.id)
+    let newColor = "#FF0000"; //RED
+    if(feature.h.id == 0) newColor = "#00FF00"; //GREEN
+    else newColor = "#0000FF"; //BLUE
+    return ({
+      clickable: false,
+      fillColor: newColor,
+      strokeWeight: 1
+    });
+  }
+
+
+  //visualizziamo i marker
+  markerGenerator()
+  {
+    this.markerList =[
+      {
+        position : {
+          lng : this.geoJsonObject.features[0].geometry.coordinates[0][0][0],
+          lat : this.geoJsonObject.features[0].geometry.coordinates[0][0][1]
+        },
+        label: String(this.geoJsonObject.features[0].properties.id),
+      },
+      {
+        position : {
+          lng : this.geoJsonObject.features[1].geometry.coordinates[0][0][0],
+          lat : this.geoJsonObject.features[1].geometry.coordinates[0][0][1]
+        },
+        label: String(this.geoJsonObject.features[1].properties.id),
+      },
+    ]
+
+  }
+
+  
+}
+  
+  
+  
+  
+  
+  
+  
+  
+
