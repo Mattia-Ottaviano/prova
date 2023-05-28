b28c77c08947e91f04f67c819f617346d9c7b77f
Mattia-Ottaviano <ottaviano.mattia@iisgalvanimi.edu.it>
2023-05-28 14:27:00 +0000

    .

--- prepVer/app.py
+++ prepVer/app.py
@@ -3,26 +3,70 @@ from flask import jsonify
 from flask import request
 from flask_pymongo import PyMongo
 from flask_cors import CORS
+import random
 
 app = Flask(__name__)
-
+CORS(app)
 # Stringa di connessione al DB
 app.config["MONGO_URI"] = "mongodb+srv://ottavianomattia:scrivania0921!1509@cluster0.jjjyhjc.mongodb.net/ninjaturtle" #Importante qui va specificato il nome del DB
 
 mongo = PyMongo(app)
 # Per rispondere alle chiamate cross origin
-CORS(app)
+
 
 @app.route('/all', methods=['GET'])
 def get_all_addresses():
     tombino = mongo.db.tombino
     output = []
-    for s in tombino.find().limit(100):
+    for s in tombino.find():
         output.append({"lat": s["lat"], "lng":s["lng"]})
                             
     return jsonify(output)
 
+
+@app.route('/raffaello', methods=['GET'])
+def get_raffaello():
+    tombino = mongo.db.tombino
+    output = []
+    for s in tombino.find():
+        output.append({"lat": s["lat"], "lng":s["lng"]})
+                            
+    return jsonify(output[random.randrange(0, len(output))])
+
+@app.route('/donatello', methods=['GET'])
+def get_donatello():
+    tombino = mongo.db.tombino
+    output = []
+    for s in tombino.find():
+        output.append({"lat": s["lat"], "lng":s["lng"]})
+                            
+    return jsonify(output[random.randrange(0, len(output))])
+
+@app.route('/michelangelo', methods=['GET'])
+def get_michelangelo():
+    tombino = mongo.db.tombino
+    output = []
+    for s in tombino.find():
+        output.append({"lat": s["lat"], "lng":s["lng"]})
+                            
+    return jsonify(output[random.randrange(0, len(output))])
+
+@app.route('/leonardo', methods=['GET'])
+def get_leonardo():
+    tombino = mongo.db.tombino
+    output = []
+    for s in tombino.find():
+        output.append({"lat": s["lat"], "lng":s["lng"]})
+                            
+    return jsonify(output[random.randrange(0, len(output))])
+
+
+
+
+
+
+
 # Checks to see if the name of the package is the run as the main package.
 if __name__ == "__main__":
     # Runs the Flask application only if the main.py file is being run.
-    app.run()
+    app.run(debug=True)
--- prepVer/verifica/src/app/app.component.ts
+++ prepVer/verifica/src/app/app.component.ts
@@ -8,32 +8,111 @@ import { Prova } from './models/prova.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   title = 'verifica';
   // google maps zoom level
   zoom: number = 8;
   fillColor: string = "#FF0000";  //Colore delle zone catastali
   markers!: Marker[];  //Vettore con tutti i marker
+  markerOptions!: google.maps.MarkerOptions;
   center: any;
-  url = "https://5000-mattiaottaviano-prova-j4k5nieowyt.ws-eu98.gitpod.io/all"
+  url = "https://5000-mattiaottaviano-prova-jb11z90n9ry.ws-eu98.gitpod.io/"
+  accendi: boolean = false
 
   constructor(public http: HttpClient) {
-    this.center={lat: 40.81132524491864, lng: -74.07094830575488};
+    this.center = { lat: 40.81132524491864, lng: -74.07094830575488 };
+
+    let iconData: google.maps.Icon = {
+      url: './assets/img/hatch.png',
+      scaledSize: new google.maps.Size(60, 60)
+    }
+    this.markerOptions = { icon: iconData }
+
   }
 
   ngOnInit(): void {
     this.markers = [];
 
-    this.http.get<Prova[]>("https://5000-mattiaottaviano-prova-j4k5nieowyt.ws-eu98.gitpod.io/all").subscribe(data =>{
-     for (let d of data) {
-      let lng = d["lat"]
-      let lat = d["lng"]
+    this.http.get<Prova[]>(this.url + "all").subscribe(data => {
+      console.log(data)
+      for (let d of data) {
+        let lng = d["lng"]
+        let lat = d["lat"]
+        let marker: Marker = new Marker(lat, lng);
+        this.markers.push(marker)
+      }
+    })
+  }
+
+  //on off marker
+  mostra() {
+    this.accendi = !this.accendi
+  }
+
+  mostraDona() {
+    this.markers = [];
+    this.http.get<Prova>(this.url + "donatello").subscribe(data => {
+      let lng = data["lng"]
+      let lat = data["lat"]
       let marker: Marker = new Marker(lat, lng);
       this.markers.push(marker)
-     }
-    })
 
+      let iconData: google.maps.Icon = {
+        url: './assets/img/Yellowicon-Tmnt-Donatelo.ico',
+        scaledSize: new google.maps.Size(60, 60)
+      }
+      this.markerOptions = { icon: iconData }
+    })
   }
-  
+
+  mostraLeo() {
+    this.markers = [];
+    this.http.get<Prova>(this.url + "leonardo").subscribe(data => {
+      let lng = data["lng"]
+      let lat = data["lat"]
+      let marker: Marker = new Marker(lat, lng);
+      this.markers.push(marker)
+
+      let iconData: google.maps.Icon = {
+        url: './assets/img/Yellowicon-Tmnt-Leonardo.ico',
+        scaledSize: new google.maps.Size(60, 60)
+      }
+      this.markerOptions = { icon: iconData }
+    })
+  }
+
+  mostraRaffa() {
+    this.markers = [];
+    this.http.get<Prova>(this.url + "raffaello").subscribe(data => {
+      let lng = data["lng"]
+      let lat = data["lat"]
+      let marker: Marker = new Marker(lat, lng);
+      this.markers.push(marker)
+
+      let iconData: google.maps.Icon = {
+        url: './assets/img/Yellowicon-Tmnt-Rafael.ico',
+        scaledSize: new google.maps.Size(60, 60)
+      }
+      this.markerOptions = { icon: iconData }
+    })
+  }
+
+  mostraMichi() {
+    this.markers = [];
+    this.http.get<Prova>(this.url + "michelangelo").subscribe(data => {
+      let lng = data["lng"]
+      let lat = data["lat"]
+      let marker: Marker = new Marker(lat, lng);
+      this.markers.push(marker)
+
+      let iconData: google.maps.Icon = {
+        url: './assets/img/Yellowicon-Tmnt-Michelangelo.ico',
+        scaledSize: new google.maps.Size(60, 60)
+      }
+      this.markerOptions = { icon: iconData }
+    })
+  }
+
+
 }
